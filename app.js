41261c0d5c6a39c63ea5016fcec2d49ff0a2cefe
Vijayasekhar Deepak <Vijayasekhar.Deepak@ducenIT.com>
2020-05-29 23:58:47 +0530

    events, computed, watch, event modifier ext.,

--- app.js
+++ app.js
@@ -4,15 +4,46 @@ new Vue({
     title: "Hello! from Vue.js",
     link: "https://google.com",
     finishedLink: "<a href='https://google.com' target='_blank'>Google</a>",
-    count: 0
+    count: 0,
+    x: 0,
+    y: 0,
+    name: "John",
+    counter1: 0,
+    counter2: 0
+  },
+  computed: {
+    countOutput: function() {
+      console.log("computed");
+      return this.counter1 > 5 ? "Greater than 5" : "Smaller than 5";
+    }
+  },
+  watch: {
+    // name should match the property in data to watch it changes
+    counter2: function(nextValue) {
+      let vm = this;
+      setTimeout(() => {
+        vm.counter2 = 0;
+      }, 2000);
+    }
   },
   methods: {
     sayHello: function () {
       this.title = "Vue.js";
       return this.title;
     },
-    increase: function() {
-        this.count++;
+    increase: function(step, event) {
+        this.count += step;
+    },
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe: function() {
+      alert("Alert!");
+    },
+    result: function() {
+      console.log("method");
+      return this.counter1 > 5 ? "Greater than 5" : "Smaller than 5";
     }
   },
 });
