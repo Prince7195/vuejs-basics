63c76cadbeba9a0b7fe49a8f7614ae534289966d
Vijayasekhar Deepak <Vijayasekhar.Deepak@ducenIT.com>
2020-06-08 11:58:58 +0530

    vue-router with router, routes, parems, query, route name and redirect

---
+++ routing-vue/src/components/user/User.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button class="btn btn-primary" @click="goHome">Go Home</button>
+        <br><br>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            goHome() {
+                this.$router.push({path: "/"})
+            }
+        }
+    }
+</script>
